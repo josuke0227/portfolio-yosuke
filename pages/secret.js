0c7e59c70ce00d37a688ba84b1626a30710b6de3
Josuke <y.motosugi0227@gmail.com>
2021-03-25 15:53:22 +1100

    Protect secret page with Redirect component

--- pages/secret.js
+++ pages/secret.js
@@ -1,17 +1,28 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import { useGetUser } from "../actions/user";
+import { useRouter } from "next/router";
+import Redirect from "../components/shared/Redirect";
 
 const Secret = () => {
   const { data, loading } = useGetUser();
+  const router = useRouter();
 
-  return (
-    <BaseLayout>
-      <BasePage>
-        <h1>I am Secret Page</h1>
-      </BasePage>
-    </BaseLayout>
-  );
+  if (loading) {
+    return <p> Loading... </p>;
+  }
+
+  if (!data) {
+    return <Redirect to="/api/v1/login" />;
+  } else {
+    return (
+      <BaseLayout>
+        <BasePage>
+          <h1>I am Secret Page</h1>
+        </BasePage>
+      </BaseLayout>
+    );
+  }
 };
 
 export default Secret;
