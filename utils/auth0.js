621485288234cc33859bba2f7ebe5d71c5eaec0c
Josuke <y.motosugi0227@gmail.com>
2021-03-26 15:02:20 +1100

    Implement server side auth

--- utils/auth0.js
+++ utils/auth0.js
@@ -26,3 +26,18 @@ export const authorizeUser = async (req, res) => {
 
   return session.user;
 };
+
+export const withAuth = (getData) => async ({ req, res }) => {
+  const session = await auth0.getSession(req);
+  if (!session || !session.user) {
+    res.writeHead(302, {
+      location: "/api/v1/login",
+    });
+    res.end();
+    return { props: {} };
+  }
+
+  const data = getData ? await getData({ req, res }, session.user) : {};
+
+  return { props: { user: session.user, ...data } };
+};
